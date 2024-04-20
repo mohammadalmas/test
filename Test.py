9550532f31f7a9089f09f23976d7461c769911ce
mohammad <mohammad.mamashli@gmail.com>
2024-04-20 20:06:44 +0330

    creat class Car

--- Test.py
+++ Test.py
@@ -1,1 +1,16 @@
 print ("Wellcome to here")
+class Car:
+    def __init__(self,model,color,year):
+        self.model = model
+        self.color = color
+        self.year = year
+        self.heater = Heater()
+    def __repr__(self):
+        return f"Model: {self.model} , Color:{self.color} , Year: {self.year}"
+    def break_(self):
+        return "This car is 'STOPING'"
+    def turn_heater_on(self,temp):
+        return self.heater.reach_request_temp()
+a = Car("benz","wight",2020)
+print(a)
+print(a.turn_heater_on(18))
